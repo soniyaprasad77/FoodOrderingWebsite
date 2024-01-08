ffca00b3e385cf1969b656e293d9500a34f5e150
soniya prasad <soniyagupta82270@gmail.com>
2024-01-08 14:01:33 +0530

    feat: Online/Offline Status

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,32 @@
 import React from "react";
 import RestaurantCard from "./RestaurantCard.js";
 import ShimmerUI from "./ShimmerUI.js";
-import { useState, useEffect} from "react";
-import {Link} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { swiggy_api_bangaluru } from "../../utils/constants.js";
+import useOnlineStatus from "../../utils/useOnlineStatus.js";
 const Body = () => {
-  const [filteredResList, setFilteredResList] = useState([]);
   const [searchText, setSearchText] = useState("");
-  // const resList  = useResData();
-  // console.log(resList);
   const [resList, setResList] = useState([]);
+  const [filteredResList, setFilteredResList] = useState([]);
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-
     const data = await fetch(swiggy_api_bangaluru);
-    // const data = await fetch(SWIGGY_API_MOTIHARI);
     const jsonData = await data.json();
     const restaurantBangaluru =
       jsonData.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
     setResList(restaurantBangaluru);
     setFilteredResList(restaurantBangaluru);
-  }
+  };
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false)
+    return <h1>You are Offline, Please check your internet connectivity</h1>;
+
 
   return resList.length === 0 ? (
     <ShimmerUI />
@@ -56,7 +58,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = resList.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating > 4.5
             );
             setResList(filteredList);
           }}
@@ -68,13 +70,15 @@ const Body = () => {
       <div div className="res-container">
         {filteredResList.map((restaurant) => (
           <Link
-          key={restaurant.info.id}
-          to={"/restaurants/" + restaurant.info.id
-        }><RestaurantCard  resData={restaurant} /></Link>
+            key={restaurant.info.id}
+            to={"/restaurants/" + restaurant.info.id}
+          >
+            <RestaurantCard resData={restaurant} />
+          </Link>
         ))}
       </div>
     </div>
   );
 };
 
-export default Body
+export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { LOGO_URL } from "../../utils/constants";
 import {Link} from "react-router-dom";
+import useOnlineStatus from "../../utils/useOnlineStatus";
 const Header = () => {
   const [btnName, setBtnName] = useState('Login')
+  const onlineStatus = useOnlineStatus();
   return (
     <>
       <div className="header">
@@ -19,6 +21,7 @@ const Header = () => {
           </div>
         <div className="nav-items">
           <ul>
+            <li>Online Status: {onlineStatus?"✔️" : "❌"}</li>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">ContactUs</Link></li>
