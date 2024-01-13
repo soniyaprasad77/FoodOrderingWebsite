9a5fd8eff03cbf041f986d35de0d9ea598acc493
soniya prasad <soniyagupta82270@gmail.com>
2024-01-13 23:24:51 +0530

    style: restaurant-menu

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,19 +6,21 @@ const RestaurantCard = (props) => {
   const { name, cuisines, area, costForTwo, avgRating } = resData.info;
   const { nextCloseTime } = resData.info.availability;
   return (
-    <div className="res-card w-[332px] m-4 rounded-xl" style={{backgroundColor:"#f0f0f0"}}>
+    <div
+      className="res-card w-[332px] m-4 rounded-xl"
+      style={{ backgroundColor: "#f0f0f0" }}
+    >
       <div className="p-4">
-      <img
-
-        className="res-logo rounded-xl "
-        src={IMG_URL + resData.info.cloudinaryImageId}
-      />
-      <h4>{name}</h4>
-      <h5>{cuisines.join(", ")}</h5>
-      <h5>{area}</h5>
-      <h5>{nextCloseTime}</h5>
-      <h5>{costForTwo}</h5>
-      <h5>{avgRating}</h5>
+        <img
+          className="res-logo rounded-xl "
+          src={IMG_URL + resData.info.cloudinaryImageId}
+        />
+        <h4 className="font-bold py-2">{name}</h4>
+        <h5>{cuisines.join(", ")}</h5>
+        <h5>{area}</h5>
+        <h5>{nextCloseTime}</h5>
+        <h5>{costForTwo}</h5>
+        <h5>{avgRating}</h5>
       </div>
     </div>
   );
