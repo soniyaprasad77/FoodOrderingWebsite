b565cff1f416573a8861a46fcbd587ab66af74f6
soniya prasad <soniyagupta82270@gmail.com>
2024-01-26 19:19:56 +0530

    Context Provider

--- src/App.js
+++ src/App.js
@@ -1,63 +1,69 @@
-import React, {Suspense, lazy} from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
-import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { useState } from "react";
 import About from "./components/About.js";
 import Contact from "./components/Contact.js";
-import Error  from "./components/Error.js";
+import Error from "./components/Error.js";
 import RestaurantMenu from "./components/RestaurantMenu.js";
 import ShimmerUI from "./components/ShimmerUI.js";
-
-const Grocery = lazy(()=>import("./components/Grocery.js"));
-
+import UserContext from "../utils/UserContext.js";
+const Grocery = lazy(() => import("./components/Grocery.js"));
 
 const App = () => {
+  const [userName, setUserName] = useState();
+  useEffect(() => {
+    const data = {
+      name: "Soniya prasad",
+    };
+    setUserName(data.name);
+  }, []);
 
   return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
+    <UserContext.Provider value={{loggedInUser:userName}}>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
   );
 };
 
 const appRouter = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
-    errorElement: <Error/>,
-    children:[
-    {
-     path: '/',
-     element:<Body/>
-    },
-    {
-      path: '/about',
-      element: <About/>
-    },
-    {
-      path: '/contact',
-      element: <Contact/>
-    }
-    , 
-    {
-      path: 'restaurants/:resId',
-      element: <RestaurantMenu/>
-    }
-    , 
-    {
-      path: '/grocery',
-      element:<Suspense fallback={<h1>Hey.. its loading buddy</h1>}> <Grocery/></Suspense>
-    }
-    , 
-    ]
-}
-])
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Hey.. its loading buddy</h1>}>
+            {" "}
+            <Grocery />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RouterProvider router={appRouter}>
-    
-  </RouterProvider>
-);
-
+root.render(<RouterProvider router={appRouter}></RouterProvider>);
