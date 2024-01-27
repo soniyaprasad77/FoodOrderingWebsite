11abf112a8914bb6037c2595dab8b218070b21ec
soniya prasad <soniyagupta82270@gmail.com>
2024-01-27 23:48:48 +0530

    style: ShimmerUI

--- src/components/ShimmerUI.js
+++ src/components/ShimmerUI.js
@@ -3,22 +3,24 @@ import React from "react";
 function ShimmerUI() {
   return (
     <>
-      <div className="shimmer-button"></div>
-      <div className="Shimmer-Container">
+      <div className="shimmer-button "></div>
+      <div className="Shimmer-Container flex flex-wrap">
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
+        <div className="shimmer-card bg-gray-200 w-[270px] h-[400px] m-4 rounded-xl"></div>
        
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
       </div>
     </>
   );
