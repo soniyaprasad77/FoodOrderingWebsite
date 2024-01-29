b78ea1425e1cf0bbda8240dd6ddb83570e446a5c
soniya prasad <soniyagupta82270@gmail.com>
2024-01-30 01:43:23 +0530

    Style: About Comp

--- src/components/About.js
+++ src/components/About.js
@@ -2,35 +2,27 @@ import UserClass from "./UserClass";
 import { Component } from "react";
 import UserContext from "../../utils/UserContext";
 class About extends Component {
-     constructor(props){
-        super(props)
-        console.log("Parent Contructor Called");
-     }
+  constructor(props) {
+    super(props);
+    console.log("Parent Contructor Called");
+  }
 
-     componentDidMount(){
-        console.log("Parent ComponentDidMount is called")
-    }
-     render(){
-        console.log("Parent Render Method is called");
-        return (
-            <>
-            <h1>This is Our About Page </h1>
-            <div className="userCalss-about">
-            <div>
-               <UserContext.Consumer>
-                  {
-                     ({loggedInUser})=>(
-                        <h1 className="font-bold py-4">{loggedInUser}</h1>
-                     )
-                  }
-               </UserContext.Consumer>
-            </div>
-            <UserClass name="Soniya Prasad" location="Motihari"/>          
-            </div>
-            </>
-        )
-     }
-
+  componentDidMount() {
+    console.log("Parent ComponentDidMount is called");
+  }
+  render() {
+    console.log("Parent Render Method is called");
+    return (
+      <div className="userCalss-about flex justify-center">
+        {/* <UserContext.Consumer>
+          {({ loggedInUser }) => (
+            <h1 className="font-bold py-4">{loggedInUser}</h1>
+          )}
+        </UserContext.Consumer> */}
+        <UserClass name="Soniya Prasad" location="Motihari" />
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import Contact from './Contact'
-class UserClass extends React.Component{
+import React from "react";
+import Contact from "./Contact";
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log(this.props.name + "child Contructor Called");
+    this.state = {
+      userInfo: {
+        name: "soni",
+        location: "here dummy",
+      },
+    };
+  }
+  async componentDidMount() {
+    // console.log(this.props.name + "Child ComponentDidMount is called")
 
-    constructor(props){
-       
-        super(props);
-       // console.log(this.props.name + "child Contructor Called");
-        this.state={
-            userInfo:{
-                name: 'soni',
-                location: 'here dummy'
-            }
-        }
-    }
-    async componentDidMount(){
-       // console.log(this.props.name + "Child ComponentDidMount is called")
-       
-        const userdata = await fetch('https://api.github.com/users/soniyaprasad77');
-        const json = await userdata.json();
-        this.setState({
-            userInfo:json
-        });
-    }
+    const userdata = await fetch("https://api.github.com/users/soniyaprasad77");
+    const json = await userdata.json();
+    this.setState({
+      userInfo: json,
+    });
+  }
 
-    componentWillUnmount(){
-        console.log("it is unmounted")
-    }
+  componentWillUnmount() {
+    console.log("it is unmounted");
+  }
 
-    render(){
-         const {name, location, avatar_url} = this.state.userInfo;
-        //console.log(this.props.name + "Child Render Called");
-        return(
-            <>
-            <div className="about-card">
-            <h1>Name: {name}</h1>
-            <h2>Location: {location}</h2>
-            <img src={avatar_url} />
-            </div>
-            </>
-        )
-    }
+  render() {
+    const { name, location, avatar_url } = this.state.userInfo;
+    //console.log(this.props.name + "Child Render Called");
+    return (
+      <>
+        <div className="about-card">
+          <h1 className="flex justify-center font-serif font-bold m-6">
+            Name: {name}
+          </h1>
+          <h2 className="flex justify-center font-serif font-bold m-6">
+            Location: {location}
+          </h2>
+          <img src={avatar_url} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default UserClass;
