e652f8b424c9b443d2269fb8984a588d815f4bad
soniya prasad <soniyagupta82270@gmail.com>
2024-01-31 01:15:05 +0530

    style: Header for smaller devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,54 +8,99 @@ import { useSelector } from "react-redux";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const onlineStatus = useOnlineStatus();
-  //const data = useContext(UserContext);
   const { loggedInUser } = useContext(UserContext);
   const cartItems = useSelector((store) => store.cart.items);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <div className="header flex justify-between bg-pink-100 shadow-xl">
-        <div className="logo-container w-24">
-          <img src={LOGO_URL} alt="logo" className="logo" />
-        </div>
-        <div className=" ">
-          <ul className="flex justify-between p-4 m-4">
-            <li className="px-6">
-              {" "}
-              Online Status: {onlineStatus ? "✔️" : "❌"}
-            </li>
-            <li className="px-6">
+      <div className="header bg-pink-100 shadow-xl p-4">
+        <div className="flex items-center justify-between">
+          <div className="logo-container">
+            <img src={LOGO_URL} alt="logo" className="logo w-24" />
+          </div>
+          <button
+            className="lg:hidden focus:outline-none"
+            onClick={toggleMenu}
+          >
+            ☰
+          </button>
+          <ul className="hidden lg:flex lg:space-x-4 lg:ml-auto lg:items-center">
+            <li>
               <Link to="/">Home</Link>
             </li>
-            <li className="px-6">
+            <li>
               <Link to="/about">About</Link>
             </li>
-            <li className="px-6">
+            <li>
               <Link to="/contact">ContactUs</Link>
             </li>
-            <li className="px-6">
+            <li>
               <Link to="/cart">
                 Add to Cart <sup>{cartItems.length}</sup>
               </Link>
             </li>
-            <li className="px-6">
+            <li>
               <Link to="grocery">Grocery</Link>
             </li>
-            <button
-              className="login-logout-btn"
-              onClick={() => {
-                btnName === "Login"
-                  ? setBtnName("Logout")
-                  : setBtnName("Login");
-              }}
-            >
-              {btnName}
-            </button>
-            <li className="px-6 font-bold">{loggedInUser}</li>
+            <li className="font-bold">{loggedInUser}</li>
+            <li>
+              <button
+                className="login-logout-btn"
+                onClick={() => {
+                  btnName === "Login"
+                    ? setBtnName("Logout")
+                    : setBtnName("Login");
+                }}
+              >
+                {btnName}
+              </button>
+            </li>
           </ul>
         </div>
+        {isMenuOpen && (
+          <ul className="lg:hidden">
+            {/* Render menu items for small screens here */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">ContactUs</Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                Add to Cart <sup>{cartItems.length}</sup>
+              </Link>
+            </li>
+            <li>
+              <Link to="grocery">Grocery</Link>
+            </li>
+            <li className="font-bold">{loggedInUser}</li>
+            <li>
+              <button
+                className="login-logout-btn"
+                onClick={() => {
+                  btnName === "Login"
+                    ? setBtnName("Logout")
+                    : setBtnName("Login");
+                }}
+              >
+                {btnName}
+              </button>
+            </li>
+          </ul>
+        )}
       </div>
     </>
   );
 };
+
 export default Header;
