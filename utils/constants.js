74af43c1f027a004c511b233f7f354ee62d2c31a
soniya prasad <soniyagupta82270@gmail.com>
2024-02-02 15:37:09 +0530

    cors issue resolved

--- utils/constants.js
+++ utils/constants.js
@@ -5,7 +5,7 @@ export const IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const SWIGGY_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.6454766&lng=84.9290699&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  " https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.6454766&lng=84.9290699&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_MENU_API = 
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.6454766&lng=84.9290699&restaurantId="
+" https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.6454766&lng=84.9290699&restaurantId="
